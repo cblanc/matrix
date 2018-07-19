4478976d264c24b572ab33ca642ebf9da6dd2129
Christopher Blanchard <cablanchard@gmail.com>
2018-07-19 23:43:48 +0100

    Test existing operations on numeric matrices only

--- tests/operations.unit.ts
+++ tests/operations.unit.ts
@@ -1,6 +1,6 @@
 import { assert } from "chai";
 import { DimensionsNotEqualError } from "../lib/errors";
-import { GenericMatrix } from "../lib/index";
+import { GenericMatrix, Matrix } from "../lib/index";
 import { equals, add, subtract } from "../lib/operations";
 
 describe("Matrix operations", () => {
@@ -26,16 +26,16 @@ describe("Matrix operations", () => {
 
 	describe("add", () => {
 		it ("adds two matrices", () => {
-			const A = new GenericMatrix<number>(2,3).fill(1);
-			const B = new GenericMatrix<number>(2,3).fill(2);
-			const C = new GenericMatrix<number>(2,3).fill(3);
+			const A = new Matrix(2,3).fill(1);
+			const B = new Matrix(2,3).fill(2);
+			const C = new Matrix(2,3).fill(3);
 			const R = add(A, B);
 			assert.isTrue(R.equals(C));
 		});
 		it ("throws an error if matrices are invalidly size", () => {
-			const A = new GenericMatrix<number>(2,3).fill(0);
-			const B = new GenericMatrix<number>(0,3).fill(0);
-			const C = new GenericMatrix<number>(2,0).fill(0);
+			const A = new Matrix(2,3).fill(0);
+			const B = new Matrix(0,3).fill(0);
+			const C = new Matrix(2,0).fill(0);
 			assert.throws(() => add(A, B), DimensionsNotEqualError);
 			assert.throws(() => add(A, C), DimensionsNotEqualError);
 		});
@@ -43,16 +43,16 @@ describe("Matrix operations", () => {
 
 	describe("subtract", () => {
 		it ("subracts two matrices", () => {
-			const A = new GenericMatrix<number>(2,3).fill(3);
-			const B = new GenericMatrix<number>(2,3).fill(2);
-			const C = new GenericMatrix<number>(2,3).fill(1);
+			const A = new Matrix(2,3).fill(3);
+			const B = new Matrix(2,3).fill(2);
+			const C = new Matrix(2,3).fill(1);
 			const R = subtract(A, B);
 			assert.isTrue(R.equals(C));
 		});
 		it ("throws an error if matrices are invalidly size", () => {
-			const A = new GenericMatrix<number>(2,3).fill(0);
-			const B = new GenericMatrix<number>(0,3).fill(0);
-			const C = new GenericMatrix<number>(2,0).fill(0);
+			const A = new Matrix(2,3).fill(0);
+			const B = new Matrix(0,3).fill(0);
+			const C = new Matrix(2,0).fill(0);
 			assert.throws(() => subtract(A, B), DimensionsNotEqualError);
 			assert.throws(() => subtract(A, C), DimensionsNotEqualError);
 		});
