39e92840c946bf21973446d918288da4e0ed8304
Christopher Blanchard <cablanchard@gmail.com>
2018-07-20 20:34:22 +0100

    Typo

--- tests/numeric_matrix.unit.ts
+++ tests/numeric_matrix.unit.ts
@@ -1,9 +1,9 @@
 import { assert } from "chai";
 import { Matrix } from "../lib/numeric_matrix";
 
-describe("GenericMatrix", () => {
+describe("Matrix", () => {
 	describe("#new", () => {
-		it ("creates a new GenericMatrix instance and assigns 0s", () => {
+		it ("creates a new Matrix instance and assigns 0s", () => {
 			const M = new Matrix(4, 4);
 			M.forEach(elem => assert.equal(elem, 0));
 		});
