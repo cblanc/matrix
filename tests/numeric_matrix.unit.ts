544fa984876d4741c46bf70143237c889e6057c6
Christopher Blanchard <cablanchard@gmail.com>
2018-08-01 18:25:29 +0100

    Implement iVector

--- tests/numeric_matrix.unit.ts
+++ tests/numeric_matrix.unit.ts
@@ -1,5 +1,6 @@
 import { assert } from "chai";
-import { Matrix } from "../lib/numeric_matrix";
+import { Matrix, RowVector, ColumnVector } from "../lib/numeric_matrix";
+import { mapIncrementer } from "./helper/util";
 
 describe("Matrix", () => {
 	describe("#new", () => {
@@ -32,6 +33,18 @@ describe("Matrix", () => {
 			const A = new Matrix(8,2).fill(3)
 			const B = new Matrix(8,2).fill(12)
 			assert.isTrue(A.scale(4).equals(B));
+		});
+	});
+
+	describe("iVector", () => {
+		it ("returns row vector", () => {
+			const A = new Matrix(2,2).map((elem, i) => i);
+			const Expected0 = new RowVector(2).fill(0);
+			const Expected1 = new RowVector(2).fill(1);
+			const V0 = A.iVector(0);
+			const V1 = A.iVector(1);
+			assert.isTrue(V0.equals(Expected0));
+			assert.isTrue(V1.equals(Expected1));
 		});
 	});
 });
